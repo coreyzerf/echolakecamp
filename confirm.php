e427e1b3a31e12fbbdb4df4506cc83d9be1cd5d7
coreyzerf <corey@zerf.ca>
2018-05-10 20:35:54 -0400

    missing </select> tag

--- confirm.php
+++ confirm.php
@@ -85,22 +85,24 @@
 							</tr><tr>
 								<td class="h">Emergency Number:</td><td><b><?php echo $camper['emergnum']; ?></b></td>
 							</tr><tr>
+								<select name="friend">
+								<?php								
+									while($row = $result->fetch_assoc()){
+										$cuserid = $row['id'];
+										$cfirst = $row['first'];
+										$clast = $row['last'];
+										echo "<option value=\"" .$cuserid . "\" >" . $cfirst . " " . $clast . "</option>\n";
+									}
+								?>
+								</select>
+							</tr><tr>
 								<td class="h">Allergies:</td><td><b><?php if (empty($camper['allergy'])){ echo "None"; } else { echo $camper['allergy']; } ?></b></td>
 							</tr><tr>
 								<td class="h">Health Concerns:</td><td><b><?php if (empty($camper['healthconcerns'])){ echo "None"; } else { echo $camper['healthconcerns']; } ?></b></td>
 							</tr><tr>
 								<td class="h">Camp:</td><td><b><?php echo $camp['season'] . " (" . date('d M Y', strtotime($camp['date'])) . " - " . date('d M Y', strtotime($camp['date']. ' + 7 days')) . ")" ; ?></b></td>
 							</tr>
-						</table>
-						<select name="friend">
-						<?php								
-							while($row = $result->fetch_assoc()){
-								$cuserid = $row['id'];
-								$cfirst = $row['first'];
-								$clast = $row['last'];
-								echo "<option value=\"" .$cuserid . "\" >" . $cfirst . " " . $clast . "</option>\n";
-							}
-						?><br>
+						</table><br>
 						<hr noshade>
 						<br>
 						<p>Please choose the payment option below:</p>
