7c99ddd1ed60c6ff5b14002bf8c6b34b2e957182
coreyzerf <corey@zerf.ca>
2018-05-10 20:31:05 -0400

    more tweaks

--- confirm.php
+++ confirm.php
@@ -91,7 +91,7 @@
 							</tr><tr>
 								<td class="h">Camp:</td><td><b><?php echo $camp['season'] . " (" . date('d M Y', strtotime($camp['date'])) . " - " . date('d M Y', strtotime($camp['date']. ' + 7 days')) . ")" ; ?></b></td>
 							</tr><tr>
-								<select class="form-control" name="friend">
+								<select name="friend">
 								<?php								
 									while($row = $result->fetch_assoc()){
 										$cuserid = $row['id'];
