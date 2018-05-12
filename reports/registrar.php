57b4f48edb504b212d8fbe8ea1fd4139681b9796
coreyzerf <corey@zerf.ca>
2018-05-11 21:57:14 -0400

    sql tweaks

--- reports/registrar.php
+++ reports/registrar.php
@@ -34,7 +34,7 @@
 	$search = array();
 	$i = 0;
 	
-	$query = "SELECT * FROM `EchoPeople` LEFT JOIN `EchoPeople` on EchoPeople.id = " . $campid . ".camperid ORDER BY gender, last";	 
+	$query = "SELECT EchoPeople.* FROM `EchoPeople` LEFT JOIN `EchoPeople` on EchoPeople.id = " . $campid . ".camperid ORDER BY gender, last";	 
 	$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
 	if($result == 0){
 		$_SESSION['fmsg'] ="Failed " . mysqli_error($connection);
