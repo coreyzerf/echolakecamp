06456b622860b7361b794db476b6898f01dfd9f4
coreyzerf <corey@zerf.ca>
2018-07-10 23:04:27 -0400

    more optin work

--- optin.php
+++ optin.php
@@ -1,15 +1,6 @@
 <?php
-	$title = "Profile";
-	include('header.php');
-	//require('connect.php');
-	//require('functions.php');
-	
-	if ( !acctverify($_SESSION['username'])){
-		session_unset();
-		$_SESSION['fmsg'] = "You are not logged in.";
-		header('Location: login.php');
-	}elseif ( isset($_POST['optin'])){ 
-
+	if ( isset($_POST['optin'])){ 
+		$email = $_POST['email'];
 		$query = "INSERT `optin` (email) VALUES ($email) ;";
         $result = mysqli_query($connection, $query);
         if($result){
@@ -17,34 +8,11 @@
 		}else{
             $_SESSION['fmsg'] = "Save failed, " . mysqli_error($connection);
 		}
-    }
-	$username = $_SESSION['username'];
-	$query = "SELECT * FROM `EchoPeople` WHERE username='$username'";
-	 
-	$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-	$row = $result->fetch_assoc();
-	if (!isset($row['id'])){
-		session_unset();
-		$_SESSION['fmsg'] = "Something went wrong";
+		header('Location: index.php');
+    }else{
 		header('Location: index.php');
 	}
 	
 	msgbox ($_SESSION['smsg'], $_SESSION['fmsg'], $_SESSION['wmsg']);
 	
-?>
-</div>
-	<div class="content">
-		<div class="profile">
-			<div>
-				<div>
-					<div class="profile">
-						<h2>Profile</h2>
-						
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-<?php
-	require('footer.php');
 ?>
